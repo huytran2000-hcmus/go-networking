a1739ae7354b690a4f2611903ffcb40d0cbfc491
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-08-05 01:26:57 +0700

    Finished chapter 4

--- udp/dial_test.go
+++ udp/dial_test.go
@@ -3,11 +3,14 @@ package udp
 import (
 	"bytes"
 	"context"
+	"errors"
 	"net"
+	"os"
 	"testing"
+	"time"
 )
 
-func TestListenPackageUDP(t *testing.T) {
+func TestDialUDP(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	srv, err := echoUDPServer(ctx, "127.0.0.1:")
@@ -15,7 +18,7 @@ func TestListenPackageUDP(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	client, err := net.ListenPacket("udp", "127.0.0.1:")
+	client, err := net.Dial("udp", srv.String())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -41,38 +44,38 @@ func TestListenPackageUDP(t *testing.T) {
 		t.Fatalf("wrote %d bytes of %d", n, l)
 	}
 
-	buf := make([]byte, 256)
-	n, addr, err := client.ReadFrom(buf)
+	ping := []byte("ping")
+	n, err = client.Write(ping)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if addr.String() != interloper.LocalAddr().String() {
-		t.Fatalf("got reply from %s, want from %s", addr, srv.String())
+	if l := len(ping); l != n {
+		t.Fatalf("wrote %d bytes of %d", n, l)
+	}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 256)
+	n, err = client.Read(buf)
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	got := buf[:n]
-	if !bytes.Equal(got, interupt) {
-		t.Errorf("got %s, want %s", got, interupt)
-	}
-
-	ping := []byte("ping")
-	_, err = client.WriteTo(ping, srv)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	n, addr, err = client.ReadFrom(buf)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if addr.String() != srv.String() {
-		t.Fatalf("got reply from %s, want from %s", addr, srv.String())
-	}
-
-	got = buf[:n]
 	if !bytes.Equal(got, ping) {
 		t.Errorf("got %s, want %s", got, ping)
+	}
+
+	err = client.SetDeadline(time.Now().Add(2 * time.Second))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = client.Read(buf)
+	if err == nil && errors.Is(err, os.ErrDeadlineExceeded) {
+		t.Errorf("expected an error")
 	}
 }
